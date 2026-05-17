7849ba76be17e557a628c0ad982c7bf4251ee795
1olecram <marcelochave059@gmail.com>
2026-05-17 19:35:19 -0300

    refactor: remocao de testes antigos

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def rastrigin(x, n, bounds):
     O mínimo global está em x = [0, 0, ..., 0] com um valor de 0.
     """
     x = np.atleast_1d(x)
-    x = np.clip(x, bounds[0], bounds[1])
+    x = np.clip(x, bounds[0], bounds[1]) # limitar posição dentro dos limites do problema (tudo abaixo de -5.12 vira -5.12 e tudo acima de 5.12 vira 5.12)
     return 10 * n + np.sum(x**2 - 10 * np.cos(2 * np.pi * x), axis=-1)
 
 def weighting_inertia(i, w_max =0.9, w_min = 0.4, iter_max = 100):
@@ -23,18 +23,10 @@ def main():
     bounds = (-5.12, 5.12) # Limites do problema
     c1 = 2
     c2 = 2
-    
-    
-    print(f"Preparando PSO para a função Rastrigin com n={n} e limites {bounds}")
         
     # Inicializando o Enxame (Swarm)
     num_particles = 30
     swarm = [Particle(n, bounds) for _ in range(num_particles)]
-    
-    print(f"\n--- Estrutura do PSO ---")
-    print(f"Enxame criado com {len(swarm)} partículas.")
-    print(f"Partícula 3 - Posição inicial: {swarm[3].position}")
-    print(f"Partícula 3 - Velocidade inicial: {swarm[3].velocity}")
 
     # Inicializa o gbest
     global_best_position = np.zeros(n)
